150fecb5cf1fdbe60c2df5085c5a61ef7cf490e9
milselarch <milselarch@gmail.com>
2017-03-17 12:22:46 +0000

    parallel camera start and Alexa response using thread

--- gpio_control.py
+++ gpio_control.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_ask import Ask, statement, convert_errors, audio
 import RPi.GPIO as GPIO
 import logging
-
+import picam_qr
+import thread
 
 GPIO.setmode(GPIO.BCM)
 
@@ -12,11 +13,10 @@ ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 @ask.intent('GPIOControlIntent', mapping={'status': 'status', 'pin': 'pin'})
-def gpio_control(status):
-    import picam_qr
-    stream_url = '%s' % result
-    #thread.start_new_thread(scan, ())
-    return statement('Turning pin {} {}'.format(pin, status))
+def gpio_control(*args):
+    #stream_url = '%s' % result
+    thread.start_new_thread(picam_qr.scan, ())
+    return statement('Scanning')
 
 if __name__ == '__main__':
     port = 5000 #the custom port you want
