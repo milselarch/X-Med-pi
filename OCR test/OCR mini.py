8b0b19a9bcb0cd97c7f3fc03c69a516d4f4886c0
milselarch <milselarch@gmail.com>
2017-05-20 07:26:39 +0000

    change OCR code for raspberry pi test use

--- OCR test/OCR mini.py
+++ OCR test/OCR mini.py
@@ -5,8 +5,13 @@ from SimpleCV import Color,Camera,Display
 import vlc
  
 image_file = '2017-03-27.png'
+#https://stackoverflow.com/questions/20020206/cannot-write-mode-rgba-as-bmp-pytesser
+#RGBA to RGB something somethign problems
 im = Image.open(image_file)
-text = image_to_string(im)
+bg = Image.new("RGB", im.size, (255,255,255))
+bg.paste(im,im)
+text = image_to_string(bg)
+
 text = image_file_to_string(image_file)
 text = image_file_to_string(image_file, graceful_errors=True)
 print "=====output=======\n"
--- OCR test/OCR test.py
+++ OCR test/OCR test.py
@@ -3,76 +3,31 @@ from pytesser import *
 
 from SimpleCV import Color,Camera,Display
 import vlc
- 
-image_file = '2017-03-27.png'
-im = Image.open(image_file)
-text = image_to_string(im)
-text = image_file_to_string(image_file)
-text = image_file_to_string(image_file, graceful_errors=True)
-print "=====output=======\n"
-print text
-
-
-import sys, os
-import Queue
-import threading
 import time
 
-from SimpleCV import Color,Camera,Display
-import vlc
+display = Display()
+camera = Camera()
 
-class scanner(threading.Thread):
-    def __init__(self):
-        self.display = Display()
-        self.camera = Camera()
-        self.queue = Queue.Queue()
-        self.scan = False
-        
-        threading.Thread.__init__(self)
+cam = camera
+#prevBarcode = None
+audio = vlc.MediaPlayer('')
 
-    def put(self):
-        self.queue.put(True)
-        
-    def run(self):
-        display = self.display
-        cam = self.camera
-        #prevBarcode = None
-        audio = vlc.MediaPlayer('')
-        
-        while display.isNotDone():
-            img = cam.getImage() #gets image from the camera
-            print img.readText()
+def getText(image):
+    bg = Image.new("RGB", image.size, (255,255,255))
+    bg.paste(image)
+    text = image_to_string(bg)
+    return text
 
+k = 0
 
-            barcode = img.findBarcode() #finds barcode data from image
-            if self.scan == True and barcode != None:
-                #if there is some data processed
-                os.system('aplay accept.wav')
-                os.system('aplay hokkiengreeting.wav')
-                barcode = barcode[0]
-                result = str(barcode.data)
+while display.isNotDone():
+    img = cam.getImage() #gets image from the camera
+    pilImage = img.getPIL()
+    print(getText(pilImage))
+    img.show()
 
-                if result[:8] == 'https://' and audio.get_state() != vlc.State.Playing:
-                    audio = vlc.MediaPlayer(result)
-                    audio.play()
-                else:
-                    gold = os.system('aplay %s' % result)
-                    gold = 1
-                    
-                barcode = [] #reset barcode data to empty set
-                self.scan = False
-                #cam.close()
-
-            else:
-                try:
-                    self.queue.get(block=False)
-                    self.scan = True
-                except Queue.Empty:
-                    pass
-
-            img.save(display) #shows the image on the screen
-
-        #prevBarcode = barcode
+    k += 1
+    print(k)
     
-if __name__ == '__main__':
-    scanner().start()
+    #time.sleep(0.5)
+    
